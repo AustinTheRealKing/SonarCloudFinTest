cd8db93be2a2f42f051182edd7a5ec0114c76bd7
AustinTheRealKing <austin.king@mymail.champlain.edu>
2019-02-12 00:07:08 -0500

    Completed getPlayerMove()

--- play.go
+++ play.go
@@ -10,7 +10,24 @@ var gameBoard Board = C4Board{turn: Black}
 
 // Find the user's next move
 func getPlayerMove() Move {
-	// YOUR CODE HERE
+	var userInput Move = 9
+	counter := 0
+	fmt.Println(gameBoard.LegalMoves())
+	fmt.Println(gameBoard.String())
+	for true{
+		if contains(gameBoard.LegalMoves(), userInput){
+			return userInput
+		} else {
+			if counter > 0{
+				fmt.Println("sorry ", userInput, " was not a valid column number")
+				fmt.Println("valid moves are:  ", gameBoard.LegalMoves())
+			}
+			fmt.Print("Enter The Column Number: ")
+			fmt.Scanln(&userInput)
+		}
+		counter++
+	}
+	return userInput
 }
 
 // Main game loop
